6cdb817b5028a50e53c92760059fa2b4b9175659
MichaelAdegboyega <omolayoade14@gmail.com>
2026-08-04 23:41:52 +0100

    changes made

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// Select the DOM elements
+
 const showPassword = document.querySelector('#show-password');
 const passwordInput = document.querySelector('#password');
 
-// Listen for a click event on the button
 showPassword.addEventListener('click', function () {
     this.classList.toggle("ph-eye-slash");
-    // Check the current type attribute
     const isHidden = passwordInput.getAttribute('type') === 'password';
-    
-    // Toggle the type attribute
+
     passwordInput.setAttribute('type', isHidden ? 'text' : 'password');
     
 });
